4178d3a9385ea063971b8c23a99a5767da7d07dd
Alliquison Matias <allyquison.matias@gmail.com>
2022-07-12 22:06:34 -0300

    Create Collor.java

---
+++ JOGO_RPG/CONTROLLERS/Collor.java
@@ -0,0 +1,23 @@
+package Controllers;
+
+public class Color {
+    public static final String RESET = "\u001B[0m";
+    public static final String YELLOW = "\u001B[33m"; 
+    public static final String RED = "\uu001B[31m";
+    public static final String BLACK = "\uu001B[30m";
+    public static final String GREEN = "\uu001B[32m";
+    public static final String CYAN = "\uu001B[36m";
+    public static final String PURPLE = "\uu001B[35m";
+    public static final String BLUE = "\uu001B[34m";
+    public static final String TESTE = "\uu001B[38m";
+    public static final String WHITE = "\uu001B[37m";
+
+    public static final String YELLOW_BACKGROUND = "\u001B[43m"; 
+    public static final String RED_BACKGROUND = "\uu001B[41m";
+    public static final String BLACK_BACKGROUND = "\uu001B[40m";
+    public static final String GREEN_BACKGROUND = "\uu001B[42m";
+    public static final String CYAN_BACKGROUND = "\uu001B[46m";
+    public static final String PURPLE_BACKGROUND = "\uu001B[45m";
+    public static final String BLUE_BACKGROUND = "\uu001B[44m";
+    public static final String WHITE_BACKGROUND = "\uu001B[47m";
+}
